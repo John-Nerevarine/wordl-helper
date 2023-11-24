cfa865ccacd10a5a3de6ae4223a322b4f5a5d212
John Nerevarine <e.m.chered@gmail.com>
2023-11-24 16:51:01 +0300

    Add alorithm for words without repeats. Refactor classes  for easy testing.

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ class Letter:
 
 
 class WordConstructor:
-    def __init__(self):
+    def __init__(self, length=None, letters=None):
+        self.length = length
+        self.letters = letters
+        self.positions = None
+
+        if not self.length:
+            self.ui_get_length()
+
+        if not self.letters:
+            self.ui_get_letters()
+
+        self.generate()
+
+    def ui_get_length(self):
         while True:
             length = input('How many letters in your word? ')
             try:
@@ -20,10 +33,12 @@ class WordConstructor:
                 print('Input number bigger than  2!\n')
                 continue
         self.length = length
+
+    def ui_get_letters(self):
         self.letters = []
 
         print('\nLet\'s check your letters.  Type "0" if it\'s no more known letters.')
-        for i in range(length):
+        for i in range(self.length):
             while True:
                 letter = input(f'Input letter {i+1}: ').upper()
                 if letter == '0':
@@ -43,7 +58,7 @@ class WordConstructor:
             self.letters.append(Letter(letter))
 
             print(f'\nLet\'s check possible positions of {self.letters[i].char}.  Type "0" if it\'s no more known positions.')
-            for j in range(length):
+            for j in range(self.length):
                 while True:
                     print()
                     if self.letters[i].positions:
@@ -53,7 +68,7 @@ class WordConstructor:
                         break
                     try:
                         pos = int(pos)
-                        if not 0 < pos <= length:
+                        if not 0 < pos <= self.length:
                             raise ValueError
                         break
                     except ValueError:
@@ -64,22 +79,38 @@ class WordConstructor:
                     print()
                     break
                 self.letters[i].add_position(pos)
-        if len(self.letters) < length:
-            delta = length - len(self.letters)
+
+        if len(self.letters) < self.length:
+            delta = self.length - len(self.letters)
             for i in range(delta):
                 self.letters.append(Letter(char='_', positions=[1, 2, 3, 4, 5]))
 
-        self.generate()
-
     def generate(self):
+        positions = {}
         for let in self.letters:
-            print(let.char, let.positions)
+            for pos in let.positions:
+                if positions.get(pos):
+                    positions[pos].append(let.char)
+                else:
+                    positions[pos] = [let.char]
+        print(positions)
+        self.positions = positions
+        self.recursion_generation(word=[], pos=1)
+
+    def recursion_generation(self, word, pos):
+        if pos > self.length:
+            print(word)
+            return
+
+        else:
+            for char in self.positions[pos]:
+                if char not in word:
+                    new_word = word.copy()
+                    new_word.append(char)
+                    self.recursion_generation(new_word, pos+1)
+            return
 
 
 if __name__ == '__main__':
     print('{:#^65}'.format('  Word Constructor  '))
     WordConstructor()
-
-
-
-
